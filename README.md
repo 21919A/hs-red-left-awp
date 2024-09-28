2ff505f3d06736184b9781ccc13a0b114b51e7cc
hatatat-dev <hatatat@glowstonemania.com>
2024-09-28 09:29:17 -0700

    [autonomous] copy from https://github.com/abpa123/red-awp-left

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# hs-template
-Template project for High Stakes VEX competition by SystemError (21919A).
+# hs-red-left-awp
+Program for VEX High Stakes competition robot that is a part of the **red** alliance, on the **left** side of the field, contributing to the **Autonomous Win Point**.
+
+## Autonomous Path
+<img src=svgs/autonomous.svg width="300" height="300"/>
--- src/main.py
+++ src/main.py
@@ -23,7 +23,36 @@ def autonomous_function():
 
     log(("Competition", "competition"), "autonomous_begin")
 
-    # Add autonomous logic here
+    # Reset odometry to the starting autonomous position
+    odometry.reset(PositionWithHeading(-1500, 600, -90))
+
+    # Then try resetting it to GPS if GPS sensor is installed and reports high quality
+    reset_odometry_to_gps()
+
+    intake_1st_stage.set_velocity(470, RPM)
+    intake_2nd_stage.set_velocity(470, RPM)
+    pid_driver.drive(-1000, True)
+    clamp.set(True)
+    pid_turner.turn(80, FRAME_HEADING_RELATIVE)
+
+    intake_1st_stage.spin(REVERSE)
+    intake_2nd_stage.spin(FORWARD)
+
+    wait(1000, MSEC)
+    reset_odometry_to_gps()
+
+    pid_driver.drive(550)
+    pid_turner.turn(60, FRAME_HEADING_RELATIVE)
+    pid_driver.drive(270)
+
+    wait(1000, MSEC)
+    reset_odometry_to_gps()
+
+    pid_turner.turn(-80, FRAME_HEADING_RELATIVE)
+    pid_driver.drive(-600, False)
+
+    wait(1000, MSEC)
+    reset_odometry_to_gps()
 
     log(("Competition", "competition"), "autonomous_end")
 
