106af09ea7db7b2573a667fb7c3c6546d750cbfd
abpa123 <aditya.banwasi@gmail.com>
2024-11-16 13:07:57 -0800

    path changes

--- src/main.py
+++ src/main.py
@@ -31,25 +31,29 @@ def autonomous_function():
 
     intake_1st_stage.set_velocity(450, RPM)
     intake_2nd_stage.set_velocity(450, RPM)
-    pid_driver.drive(-1000, True)
+    pid_driver.drive(-1080, True)
     clamp.set(True)
-    pid_turner.turn(80, FRAME_HEADING_RELATIVE)
+    pid_turner.turn(70, FRAME_HEADING_RELATIVE)
+
+
+    wait(1000, MSEC)
+    reset_odometry_to_gps()
+    
+    pid_driver.drive(400)
 
     intake_1st_stage.spin(REVERSE)
     intake_2nd_stage.spin(FORWARD)
+    pid_driver.drive(150)
 
-    wait(1000, MSEC)
-    reset_odometry_to_gps()
-
-    pid_driver.drive(550)
     pid_turner.turn(105, FRAME_HEADING_RELATIVE)
     pid_driver.drive(270)
 
-    wait(1000, MSEC)
+    wait(500, MSEC)
     reset_odometry_to_gps()
     wait(50, MSEC)
-    intake_2nd_stage.stop()
-    pid_turner.turn(-80, FRAME_HEADING_RELATIVE)
+    # intake_2nd_stage.stop()
+    pid_turner.turn(-75, FRAME_HEADING_RELATIVE)
+    pid_driver.drive(50)
     # pid_driver.drive(-900, False)
 
     wait(1000, MSEC)
